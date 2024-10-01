416757e5fa51358a13145475b34ef02a9b3a9aff
pedrochatelain <pedrochatelain@gmail.com>
2024-09-30 21:56:35 -0300

    display image when tapping movie

--- app/src/main/java/com/example/themoviedatabase/ui/view/DetailsMovieScreen.kt
+++ app/src/main/java/com/example/themoviedatabase/ui/view/DetailsMovieScreen.kt
@@ -1,25 +1,32 @@
 package com.example.themoviedatabase.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.themoviedatabase.ui.viewmodel.DetailsMovieViewModel
 
 @Composable
 fun DetailsMovieScreen(idMovie: Int, viewModel: DetailsMovieViewModel = hiltViewModel()) {
-    viewModel.getMovieDetails(idMovie)
-    if (viewModel.details != null) {
+    viewModel.loadMovie(idMovie)
+    if (viewModel.isImageLoaded && viewModel.isDetailsLoaded) {
         Column {
+            Row {
+                Image(bitmap = viewModel.image, modifier = Modifier.width(440.dp).height(220.dp), contentDescription = "")
+            }
             Text(text = "Titulo", fontWeight = FontWeight.Bold, fontSize = 20.sp)
             Text(viewModel.details!!.titulo)
             Text(text = "Resumen", fontWeight = FontWeight.Bold, fontSize = 20.sp)
--- app/src/main/java/com/example/themoviedatabase/ui/viewmodel/DetailsMovieViewModel.kt
+++ app/src/main/java/com/example/themoviedatabase/ui/viewmodel/DetailsMovieViewModel.kt
@@ -1,29 +1,54 @@
 package com.example.themoviedatabase.ui.viewmodel
 
+import android.graphics.BitmapFactory
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.themoviedatabase.data.Repository
 import com.example.themoviedatabase.data.dto.DetailsMovie
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
 import javax.inject.Inject
+
 
 @HiltViewModel
 class DetailsMovieViewModel @Inject constructor(private val repository: Repository): ViewModel() {
 
-    var loading by mutableStateOf(false)
-    var isDisplayingMovie: Boolean = false
+    var isDetailsLoaded by mutableStateOf(false)
+    var isImageLoaded by mutableStateOf(false)
     var details by mutableStateOf<DetailsMovie?>(null)
+    var image by mutableStateOf(ImageBitmap(1, 1))
 
-    fun getMovieDetails(id: Int) {
-        loading = true
-        viewModelScope.launch {
+    private fun loadDetails(id: Int): Job {
+        return viewModelScope.launch {
             details = repository.getMovieDetails(id)
-            loading = false
-            isDisplayingMovie = true
+            isDetailsLoaded = true
+        }
+    }
+
+    private fun loadImage(poster: String): Job {
+        return viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val url = URL("https://image.tmdb.org/t/p/w500/${poster}")
+                val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                image = bitmap.asImageBitmap()
+                isImageLoaded = true
+            }
+        }
+    }
+
+    fun loadMovie(id: Int) {
+        viewModelScope.launch {
+            loadDetails(id).join()
+            loadImage(details!!.portada)
         }
     }
 
