505c867b505ab124d2abc10f08c98d55d7d96e77
pedrochatelain <pedrochatelain@gmail.com>
2024-10-09 16:32:57 -0300

    fix search box to top

--- app/src/main/java/com/example/themoviedatabase/ui/view/MoviesScreen.kt
+++ app/src/main/java/com/example/themoviedatabase/ui/view/MoviesScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -55,8 +56,13 @@ fun MoviesScreen(viewModel: MainViewModel = hiltViewModel(), onMovieClick: (movi
             when {
                 viewModel.loading -> CircularProgressIndicator()
                 viewModel.moviesLoaded -> {
-                    BuscadorPeliculas(viewModel)
-                    ListOfMovies(viewModel, onMovieClick)
+                    Column(
+                      verticalArrangement = Arrangement.Top,
+                      modifier = Modifier.fillMaxHeight()
+                    ) {
+                        BuscadorPeliculas(viewModel)
+                        ListOfMovies(viewModel, onMovieClick)
+                    }
                 }
                 viewModel.error -> ErrorScreen(viewModel)
             }
